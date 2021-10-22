a7aa36ed3920d0c0faacb9b6cdb8e8d2d943150d
belikova-julia <alistami38@gmail.com>
2021-10-23 00:20:06 +0300

    Scripted auto-commit on change (2021-10-23 00:20:06) by gitwatch.sh

--- src/main/java/application/DelayStatisticApp.java
+++ src/main/java/application/DelayStatisticApp.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class DelayStatisticApp {
     public static void main(String[] args) throws IOException, InterruptedException, ClassCastException {
         if (args.length != 3) {
-            System.out.println("");
+            System.err.println("");
             System.exit(-1);
         }
     }
