efd8ecaf8b199c328adc671ee2fe2b72614b79b8
belikova-julia <alistami38@gmail.com>
2021-10-23 12:10:30 +0300

    Scripted auto-commit on change (2021-10-23 12:10:30) by gitwatch.sh

--- src/main/java/application/DelayStatisticApp.java
+++ src/main/java/application/DelayStatisticApp.java
@@ -2,6 +2,8 @@ package application;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
@@ -11,13 +13,24 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import java.io.IOException;
 
 public class DelayStatisticApp {
-    public static void main(String[] args) throws IOException, InterruptedException, ClassCastException {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassCastException, ClassNotFoundException {
         if (args.length != 3) {
             System.err.println("");
             System.exit(-1);
         }
-        JobConf job = new JobConf(new Configuration(), Job.class);
+        Job job = Job.getInstance();
         job.setJarByClass(DelayStatisticApp.class);
-        MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, FlightMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[0]),
+                TextInputFormat.class, FlightMapper.class);
+        MultipleInputs.addInputPath(job, new Path(args[1]),
+                TextInputFormat.class, AeroportMapper.class);
+        job.setPartitionerClass(AeroportPartitioner.class);
+        job.setGroupingComparatorClass(AeroportGroupingComparator.class);
+        job.setReducerClass(AeroportReducer.class);
+        job.setMapOutputKeyClass(AeroportWritableComparable.class);
+        job.setMapOutputValueClass(Text.class);
+        FileOutputFormat.setOutputPath(job, new Path(args[2]));
+        job.setNumReduceTasks(2);
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
