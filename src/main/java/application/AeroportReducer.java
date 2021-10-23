c8c9a7bed470826e2d25ff2e583bc6761984f06c
belikova-julia <alistami38@gmail.com>
2021-10-23 10:11:25 +0300

    Scripted auto-commit on change (2021-10-23 10:11:25) by gitwatch.sh

--- src/main/java/application/AeroportReducer.java
+++ src/main/java/application/AeroportReducer.java
@@ -23,5 +23,7 @@ public class AeroportReducer extends Reducer<AeroportWritableComparable, Text, T
             min = Float.min(min, delay);
             count += 1;
         }
+        Text resultStatistics = new Text(String.format("max: %f, min: %f, average: %f", min, max, sum/count));
+        if 
     }
 }
