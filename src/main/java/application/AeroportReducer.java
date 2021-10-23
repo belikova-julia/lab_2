3d71ca34e7c718662d360b540d42ef2a082cd5a8
belikova-julia <alistami38@gmail.com>
2021-10-23 10:02:41 +0300

    Scripted auto-commit on change (2021-10-23 10:02:41) by gitwatch.sh

--- src/main/java/application/AeroportReducer.java
+++ src/main/java/application/AeroportReducer.java
@@ -1,6 +1,9 @@
 package application;
 
-public class AeroportReducer extends Reducer<FlightWritableComparable, Text, Text, Text> {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class AeroportReducer extends Reducer<AeroportWritableComparable, Text, Text, Text> {
     @Override
-    
+    protected void reduce(AeroportWritableComparable key, Iterable<Text> values, Context context) throws
 }
