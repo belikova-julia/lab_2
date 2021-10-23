537fe2693cba92c0414fb9ab60c82e60caccf07b
belikova-julia <alistami38@gmail.com>
2021-10-23 09:20:51 +0300

    Scripted auto-commit on change (2021-10-23 09:20:51) by gitwatch.sh

--- src/main/java/application/AeroportMapper.java
+++ src/main/java/application/AeroportMapper.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class AeroportMapper extends Mapper<LongWritable, Text, AeroportWritableComparable, Text> {
     private static final String DELIMETER = ",";
-    private static final int 
+    private static final int CODE_COLOUMN
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
