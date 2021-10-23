6b239bb10f5c9fc118b224acdff57bd5f8d5b682
belikova-julia <alistami38@gmail.com>
2021-10-23 09:19:07 +0300

    Scripted auto-commit on change (2021-10-23 09:19:07) by gitwatch.sh

--- src/main/java/application/AeroportMapper.java
+++ src/main/java/application/AeroportMapper.java
@@ -1,4 +1,19 @@
 package application;
 
-public class AeroportMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class AeroportMapper extends Mapper<LongWritable, Text, AeroportWritableComparable, Text> {
+    private static final String DELIMETER = ",";
+    private static final int 
+
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (key.get() > 0) {
+
+        }
+    }
 }
