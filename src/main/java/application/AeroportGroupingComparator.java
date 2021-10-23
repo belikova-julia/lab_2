5128901f43575f4eb06116e36b1e150aea9b4b11
belikova-julia <alistami38@gmail.com>
2021-10-23 09:59:20 +0300

    Scripted auto-commit on change (2021-10-23 09:59:20) by gitwatch.sh

--- src/main/java/application/AeroportGroupingComparator.java
+++ src/main/java/application/AeroportGroupingComparator.java
@@ -1,9 +1,19 @@
 package application;
 
+import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AeroportGroupingComparator extends WritableComparator {
+
     public AeroportGroupingComparator() {
         super(AeroportWritableComparable.class, true);
+    }
+
+    @Override
+    public int compare(WritableComparable aeroport1, WritableComparable aeroport2) {
+        AeroportWritableComparable a1 = (AeroportWritableComparable)aeroport1;
+        AeroportWritableComparable a2 = (AeroportWritableComparable)aeroport2;
+
+        return Integer.compare(a1.getAeroportID(), a2.getAeroportID());
     }
 }
