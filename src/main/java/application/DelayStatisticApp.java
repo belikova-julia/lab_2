b9ff20ec26c97f5ffc7b0068513d16147fe1a5cb
belikova-julia <alistami38@gmail.com>
2021-10-23 12:02:33 +0300

    Scripted auto-commit on change (2021-10-23 12:02:33) by gitwatch.sh

--- src/main/java/application/DelayStatisticApp.java
+++ src/main/java/application/DelayStatisticApp.java
@@ -3,10 +3,11 @@ package application;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapred.JobConf;
-
-import org.apache.hadoop.mapred.TextInputFormat;
-import org.apache.hadoop.mapred.lib.MultipleInputs;
 import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+
+
 import java.io.IOException;
 
 public class DelayStatisticApp {
