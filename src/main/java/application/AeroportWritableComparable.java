2a4c2c141f43169927b816d640e5190f5f0c3b4f
belikova-julia <alistami38@gmail.com>
2021-10-23 08:55:29 +0300

    Scripted auto-commit on change (2021-10-23 08:55:29) by gitwatch.sh

--- src/main/java/application/AeroportWritableComparable.java
+++ src/main/java/application/AeroportWritableComparable.java
@@ -1,4 +1,25 @@
 package application;
 
-public class AeroportWritableComparable {
+import org.apache.hadoop.io.WritableComparable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AeroportWritableComparable implements WritableComparable {
+
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
