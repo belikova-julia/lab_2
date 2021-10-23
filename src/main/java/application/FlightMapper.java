c165dbf9f966066b0f4088d9449e5894bc58799f
belikova-julia <alistami38@gmail.com>
2021-10-23 09:37:02 +0300

    Scripted auto-commit on change (2021-10-23 09:37:02) by gitwatch.sh

--- src/main/java/application/FlightMapper.java
+++ src/main/java/application/FlightMapper.java
@@ -1,4 +1,12 @@
 package application;
 
-public class FlightMapper {
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class FlightMapper extends Mapper<LongWritable, Text, AeroportWritableComparable, Text> {
+    private static final String DELIMETER = ",";
+    private static final int DATA_TYPE = 1;
+    private static final int DEST_AEROPORT_ID_COLUMN = 14;
+    private static final int 
 }
